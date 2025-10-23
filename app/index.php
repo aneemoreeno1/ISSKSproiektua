3a93d055fff636211684faf5d2b7d06ede40d28a
Yoelitoh <81594683+Yoelitoh@users.noreply.github.com>
2025-10-23 10:14:49 +0200

    Index berrituta

--- app/index.php
+++ app/index.php
@@ -1,6 +1,5 @@
 <?php
 echo '<h1>Yeah, it works!</h1>';
-// phpinfo();
 
 $hostname = "db";
 $username = "admin";
@@ -9,26 +8,31 @@ $db = "database";
 
 $conn = mysqli_connect($hostname, $username, $password, $db);
 if ($conn->connect_error) {
-	die("Database connection failed: " . $conn->connect_error);
+    die("Database connection failed: " . $conn->connect_error);
 }
 
 $query = mysqli_query($conn, "SELECT * FROM usuarios")
-	or die(mysqli_error($conn));
+    or die(mysqli_error($conn));
 
-echo '<h2>Erabiltzaileak:</h2>';
+echo "<h2>Erabiltzaileak:</h2>";
+echo "<table border='1'>";
+echo "<tr><th>ID</th><th>Izena</th><th> </th></tr>";
 while ($row = mysqli_fetch_array($query)) {
-	echo
-		"<tr>
-			<td>{$row['id']}</td>
-			<td>{$row['nombre']}</td>
-			<td>
-				<a href='show_user.php?user={$row['id']}'>Ikusi</a> | 
-				<a href='modify_user.php?user={$row['id']}'>Editatu</a>
-			</td>
-		</tr>";
+    echo "<tr>";
+    echo "<td>{$row['id']}</td>";
+    echo "<td>{$row['nombre']}</td>";
+    echo "<td>
+            <a href='show_user.php?user={$row['id']}'>Ikusi</a> | 
+            <a href='modify_user.php?user={$row['id']}'>Editatu</a>
+          </td>";
+    echo "</tr>";
 }
+echo "</table>";
 
 // Estekak
-echo '<p><a href="login.php">SaioaHasi</a> | <a href="register.php">Erregistratu</a></p> | <a href="items.php">Pelikulak ikusi</a></p>';
+echo '<p>';
+echo '<a href="login.php">Saioa Hasi</a> | ';
+echo '<a href="register.php">Erregistratu</a> | ';
+echo '<a href="items.php">Pelikulak Ikusi</a>';
+echo '</p>';
 ?>
-
