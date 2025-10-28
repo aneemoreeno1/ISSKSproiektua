c00d0ab16a42781b7ebcd2a4f2b3a01e72180425
Yoelitoh <81594683+Yoelitoh@users.noreply.github.com>
2025-10-28 12:27:11 +0100

    Update items.php

--- app/items.php
+++ app/items.php
@@ -1,7 +1,5 @@
 <?php
 
-// items.php - Elementuen zerrenda
-
 $hostname = "db";
 $username = "admin";
 $password = "test";
@@ -16,44 +14,38 @@ if ($conn->connect_error) {
 $query = mysqli_query($conn, "SELECT * FROM pelikulak") or die(mysqli_error($conn));
 ?>
 
-<!DOCTYPE html>
-<html lang="eu">
-<head>
-    <meta charset="UTF-8">
-    <title>Pelikulak</title>
-    <link rel="stylesheet" href="style.css">
-</head>
 <body>
-    <h1>Pelikulak</h1>
-    
-    <?php
-    // Elementuak erakusten
-    if (mysqli_num_rows($query) > 0) {
-        echo "<table>";
-        echo "<tr><th>ID</th><th>Izena</th><th>Ekintzak</th></tr>";
+    <div class="wrapper">
+        <h1>Pelikulak</h1>
         
-        while ($row = mysqli_fetch_array($query)) {
-            echo "<tr>";
-            echo "<td>{$row['id']}</td>";
-            echo "<td>{$row['izena']}</td>";
-            echo "<td>
-                    <a href='show_item.php?item={$row['id']}'>Ikusi</a> | 
-                    <a href='modify_item.php?item={$row['id']}'>Editatu</a> | 
-                    <a href='delete_item.php?item={$row['id']}'>Ezabatu</a>
-                  </td>";
-            echo "</tr>";
+        <?php
+        // Elementuak erakusten
+        if (mysqli_num_rows($query) > 0) {
+            echo "<table>";
+            echo "<tr><th>ID</th><th>Izena</th><th>Ekintzak</th></tr>";
+            
+            while ($row = mysqli_fetch_array($query)) {
+                echo "<tr>";
+                echo "<td>{$row['id']}</td>";
+                echo "<td>{$row['izena']}</td>";
+                echo "<td>
+                        <a href='show_item.php?item={$row['id']}'>Ikusi</a> | 
+                        <a href='modify_item.php?item={$row['id']}'>Editatu</a> | 
+                        <a href='delete_item.php?item={$row['id']}'>Ezabatu</a>
+                      </td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "<p>Ez daude pelikularik.</p>";
         }
-        echo "</table>";
-    } else {
-        echo "<p>Ez daude pelikularik.</p>";
-    }
 
-    mysqli_close($conn);
-    ?>
-    
-    <p>
-        <button id="item_add_submit" type="button" onclick="window.location.href='add_item.php'">Pelikula Berria Gehitu</button>
-        <button id="home" type="button" onclick="window.location.href='index.php'">Hasierara Bueltatu</button>
-    </p>
+        mysqli_close($conn);
+        ?>
+        
+        <div class="botoiak">
+            <button id="item_add_submit" type="button" onclick="window.location.href='add_item.php'">Pelikula Berria Gehitu</button>
+            <button id="home" type="button" onclick="window.location.href='index.php'">Hasierara Bueltatu</button>
+        </div>
+    </div>
 </body>
-</html>
