7ab71b896b11584dc383b9cde109a6df7d1d7be6
Xinyan05617 <166069420+Xinyan05617@users.noreply.github.com>
2025-10-28 10:18:03 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,9 +1,6 @@
 <?php
 session_start(); 
 
-
-    
-
 $hostname = "db";
 $username = "admin";
 $password = "test";
@@ -16,34 +13,37 @@ if ($conn->connect_error) {
 
 $query = mysqli_query($conn, "SELECT * FROM usuarios")
     or die(mysqli_error($conn));
+<!DOCTYPE html>
+<html lang="eu">
+<head>
+    <meta charset="UTF-8">
+    <title>Erabiltzaileak</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-echo "
-        
-        <h1>Erabiltzaileak:</h1>";
-echo "<table border='1px solid #ddd'>";
-echo "<tr><th>ID</th><th>Izena</th><th> </th></tr>";
-while ($row = mysqli_fetch_array($query)) {
-    echo
-        "<tr>
-            <td>{$row['id']}</td>
-            <td>{$row['nombre']}</td>
-            <td>
-                <a href='show_user.php?user={$row['id']}'>Ikusi</a>";
-    
-    // SOILIK saioa duen erabiltzaileak editatu ahal du bere datuak
-    if (isset($_SESSION['user_id']) && $_SESSION['user_id'] == $row['id']) {
-        echo " | <a href='modify_user.php?user={$row['id']}'>Editatu</a>";  
-    }
-    
-    echo "</td>
-        </tr>";
-}
-echo "</table>";
+    <h1>Erabiltzaileak:</h1>
+    <table>
+        <tr><th>ID</th><th>Izena</th><th> </th></tr>
+        <?php while ($row = mysqli_fetch_array($query)) { ?>
+            <tr>
+                <td><?= $row['id'] ?></td>
+                <td><?= $row['nombre'] ?></td>
+                <td>
+                    <a href="show_user.php?user=<?= $row['id'] ?>">Ikusi</a>
+                    <?php if (isset($_SESSION['user_id']) && $_SESSION['user_id'] == $row['id']) { ?>
+                        | <a href="modify_user.php?user=<?= $row['id'] ?>">Editatu</a>
+                    <?php } ?>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
 
-// Estekak
-echo '<p>';
-echo '<button onclick="window.location.href=\'login.php\'">Saioa Hasi</button> ';
-echo '<button onclick="window.location.href=\'register.php\'">Erregistratu</button> ';
-echo '<button onclick="window.location.href=\'items.php\'">Pelikulak Ikusi</button> ';
-echo '</p>';
-?>
+    <p class="botoiak">
+        <button onclick="window.location.href='login.php'">Saioa Hasi</button>
+        <button onclick="window.location.href='register.php'">Erregistratu</button>
+        <button onclick="window.location.href='items.php'">Pelikulak Ikusi</button>
+    </p>
+
+</body>
+</html>
