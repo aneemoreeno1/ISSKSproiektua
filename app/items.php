369dfce846c3f400c4b0b7d5ef1807766fa89485
Yoelitoh <81594683+Yoelitoh@users.noreply.github.com>
2025-10-28 12:32:25 +0100

    Update items.php

--- app/items.php
+++ app/items.php
@@ -17,35 +17,35 @@ $query = mysqli_query($conn, "SELECT * FROM pelikulak") or die(mysqli_error($con
 <body>
     <div class="wrapper">
         <h1>Pelikulak</h1>
-        
-        <?php
-        // Elementuak erakusten
-        if (mysqli_num_rows($query) > 0) {
-            echo "<table>";
-            echo "<tr><th>ID</th><th>Izena</th><th>Ekintzak</th></tr>";
-            
-            while ($row = mysqli_fetch_array($query)) {
-                echo "<tr>";
-                echo "<td>{$row['id']}</td>";
-                echo "<td>{$row['izena']}</td>";
-                echo "<td>
-                        <a href='show_item.php?item={$row['id']}'>Ikusi</a> | 
-                        <a href='modify_item.php?item={$row['id']}'>Editatu</a> | 
-                        <a href='delete_item.php?item={$row['id']}'>Ezabatu</a>
-                      </td>";
-                echo "</tr>";
-            }
-            echo "</table>";
-        } else {
-            echo "<p>Ez daude pelikularik.</p>";
-        }
 
-        mysqli_close($conn);
-        ?>
-        
+        <?php if ($query && mysqli_num_rows($query) > 0): ?>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Izena</th>
+                    <th>Ekintzak</th>
+                </tr>
+                <?php while ($row = mysqli_fetch_array($query)): ?>
+                    <tr>
+                        <td><?= $row['id'] ?></td>
+                        <td><?= $row['izena'] ?></td>
+                        <td>
+                            <a href="show_item.php?item=<?= $row['id'] ?>">Ikusi</a> | 
+                            <a href="modify_item.php?item=<?= $row['id'] ?>">Editatu</a> | 
+                            <a href="delete_item.php?item=<?= $row['id'] ?>">Ezabatu</a>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            </table>
+        <?php else: ?>
+            <p style="text-align:center;">Ez daude pelikularik.</p>
+        <?php endif; ?>
+
         <div class="botoiak">
-            <button id="item_add_submit" type="button" onclick="window.location.href='add_item.php'">Pelikula Berria Gehitu</button>
-            <button id="home" type="button" onclick="window.location.href='index.php'">Hasierara Bueltatu</button>
+            <button type="button" onclick="window.location.href='add_item.php'">Pelikula Berria Gehitu</button>
+            <button type="button" onclick="window.location.href='index.php'">Hasierara Bueltatu</button>
         </div>
     </div>
 </body>
+
+<?php mysqli_close($conn); ?>
