f439bb061c5b6bb88f6d411968a72a57debb9d6a
Yoelitoh <81594683+Yoelitoh@users.noreply.github.com>
2025-10-28 10:58:22 +0100

    Update add_item.php

--- app/add_item.php
+++ app/add_item.php
@@ -1,6 +1,6 @@
 <?php
-
 // add_item.php - Pelikula berria gehitu
+
 $hostname = "db";
 $username = "admin";
 $password = "test";
@@ -27,122 +27,109 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     exit;
 }
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
     <title>Pelikula berria gehitu</title>
+    <link rel="stylesheet" href="style.css">
     <script>
         function bakarrikLetrak(testua) {
-            var patroia = /^[A-Za-zÑñ\s]+$/;
-            return patroia.test(testua);
+            return /^[A-Za-zÑñ\s]+$/.test(testua);
         }
 
         function bakarrikZenbakiak(testua) {
-            var patroia = /^[0-9]+$/;
-            return patroia.test(testua);
+            return /^[0-9]+$/.test(testua);
         }
 
         function bakarrikLetrakEtaZenbakiak(testua) {
-            var patroia = /^[A-Za-zÑñ0-9\s.,!?¡¿()-]+$/;
-            return patroia.test(testua);
+            return /^[A-Za-zÑñ0-9\s.,!?¡¿()-]+$/.test(testua);
         }
 
         function gutxienezLetraBat(testua) {
-            var patroia = /[A-Za-zÑñ]/;
-            return patroia.test(testua);
-        }
-
-        function karaktereArruntaK(testua) {
-            var patroia = /^[A-Za-zÑñ0-9\s.,!?¡¿()-]+$/;
-            return patroia.test(testua);
+            return /[A-Za-zÑñ]/.test(testua);
         }
 
         function datuakEgiaztatu() {
-            // Izena: ez hutsa eta letrak/zenbakiak onartu
-            var izena = document.add_form.izena.value;
+            var izena = document.item_add_form.izena.value;
             if (izena.length < 1) {
-                window.alert("Izenak ezin du hutsik egon");
+                alert("Izenak ezin du hutsik egon");
                 return false;
-            } 
-            else if (!bakarrikLetrakEtaZenbakiak(izena)) {
-                window.alert("Izenak soilik letrak, zenbakiak eta karaktere arruntak izan behar ditu");
+            } else if (!bakarrikLetrakEtaZenbakiak(izena)) {
+                alert("Izenak soilik letrak, zenbakiak eta karaktere arruntak izan behar ditu");
                 return false;
-            }
-            else if (!gutxienezLetraBat(izena)) {
-                window.alert("Izenak gutxienez letra bat izan behar du");
+            } else if (!gutxienezLetraBat(izena)) {
+                alert("Izenak gutxienez letra bat izan behar du");
                 return false;
             }
 
-            // Deskribapena: luzera maximoa egiaztatu (aukerakoa)
-            var deskribapena = document.add_form.deskribapena.value;
+            var deskribapena = document.item_add_form.deskribapena.value;
             if (deskribapena.length > 500) {
-                window.alert("Deskribapenak ezin du 500 karaktere baino gehiago izan");
+                alert("Deskribapenak ezin du 500 karaktere baino gehiago izan");
                 return false;
             }
 
-            // Urtea: zenbaki arrunta eta arrazoizko tartea
-            var urtea = document.add_form.urtea.value;
+            var urtea = document.item_add_form.urtea.value;
             if (urtea !== "") {
                 if (!bakarrikZenbakiak(urtea)) {
-                    window.alert("Urteak zenbaki osoa izan behar du");
+                    alert("Urteak zenbaki osoa izan behar du");
                     return false;
                 }
-                
                 var urteZenbakia = parseInt(urtea);
                 var gaurkoUrtea = new Date().getFullYear();
-                
-                if (urteZenbakia < 1888) { // Lehenengo pelikularen urtea
-                    window.alert("Urtea ez da egokia. 1888 baino handiagoa izan behar da");
+                if (urteZenbakia < 1888) {
+                    alert("Urtea ez da egokia. 1888 baino handiagoa izan behar da");
                     return false;
                 }
-                
                 if (urteZenbakia > gaurkoUrtea + 5) {
-                    window.alert("Urtea ez da egokia. Ezin da etorkizuneko 5 urte baino gehiago izan");
+                    alert("Urtea ez da egokia. Ezin da etorkizuneko 5 urte baino gehiago izan");
                     return false;
                 }
             }
 
-            // Egilea: letrak eta karaktere arruntak baino ez (aukerakoa)
-            var egilea = document.add_form.egilea.value;
+            var egilea = document.item_add_form.egilea.value;
             if (egilea !== "" && !bakarrikLetrakEtaZenbakiak(egilea)) {
-                window.alert("Egileak soilik letrak, zenbakiak eta karaktere arruntak izan behar ditu");
+                alert("Egileak soilik letrak, zenbakiak eta karaktere arruntak izan behar ditu");
                 return false;
-            }
-            else if (!gutxienezLetraBat(egilea)) {
-                window.alert("Egileak gutxienez letra bat izan behar du");
+            } else if (egilea !== "" && !gutxienezLetraBat(egilea)) {
+                alert("Egileak gutxienez letra bat izan behar du");
                 return false;
             }
 
-            // Generoa: letrak baino ez (aukerakoa)
-            var generoa = document.add_form.generoa.value;
+            var generoa = document.item_add_form.generoa.value;
             if (generoa !== "" && !bakarrikLetrak(generoa)) {
-                window.alert("Generoak soilik letrak izan behar ditu");
+                alert("Generoak soilik letrak izan behar ditu");
                 return false;
-            }
-            else if (!gutxienezLetraBat(generoa)) {
-                window.alert("Generoak gutxienez letra bat izan behar du");
+            } else if (generoa !== "" && !gutxienezLetraBat(generoa)) {
+                alert("Generoak gutxienez letra bat izan behar du");
                 return false;
             }
 
-            // Datuak guztiz ongi badaude
             return true;
         }
     </script>
 </head>
 <body>
-<h1>Pelikula berria gehitu</h1>
-    <form id="add_form" name="add_form" method="POST" onsubmit="return datuakEgiaztatu()">
-        <label for="izena">Izena:</label> <br> <input type="text" name="izena" required><br><br>
-        <label for="deskribapena">Deskribapena:</label> <br> <textarea name="deskribapena" rows="4" cols="50"></textarea><br><br>
-        <label for="urtea">Urtea:</label> <br> <input type="number" name="urtea" value="<?php echo date('Y'); ?>"><br><br>
-        <label for="egilea">Egilea:</label> <br> <input type="text" name="egilea"><br><br>
-        <label for="generoa">Generoa:</label> <br> <input type="text" name="generoa"><br><br>
+    <h1>Pelikula berria gehitu</h1>
 
-        <button type="submit" value="Gehitu">Gehitu</button>
-        <button type="button" id="items.php" onclick="window.location.href='items.php'">Atzera</button>
+    <form id="item_add_form" name="item_add_form" method="POST" onsubmit="return datuakEgiaztatu()">
+        <label for="izena">Izena:</label><br>
+        <input type="text" name="izena" required><br><br>
+
+        <label for="deskribapena">Deskribapena:</label><br>
+        <textarea name="deskribapena" rows="4" cols="50"></textarea><br><br>
+
+        <label for="urtea">Urtea:</label><br>
+        <input type="number" name="urtea" value="<?php echo date('Y'); ?>"><br><br>
+
+        <label for="egilea">Egilea:</label><br>
+        <input type="text" name="egilea"><br><br>
+
+        <label for="generoa">Generoa:</label><br>
+        <input type="text" name="generoa"><br><br>
+
+        <button id="item_add_submit" type="submit">Gehitu</button>
+        <button type="button" onclick="window.location.href='items.php'">Atzera</button>
     </form>
-
 </body>
 </html>
